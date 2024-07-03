790c1dabb32642644af4f4cb0a63a92c4287e27e
Fujioka <tsuyoshi@DESKTOP-PDBMBRQ>
2024-07-03 17:08:33 -0300

    Criação de mensagem de erro para arquivo com mesmo nome

--- send_smart/csvs/views.py
+++ send_smart/csvs/views.py
@@ -1,36 +1,41 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .forms import CsvModelForm
 from .models import Csv
 import csv
 from django.contrib.auth.models import User
 from contatos.models import Contato
+from django.contrib import messages
+
 # Create your views here.
 
 
 def upload_file_view(request):
     form = CsvModelForm (request.POST or None, request.FILES or None)
-    if form.is_valid():        
-        model=Csv()
-        model.arquivo=form.cleaned_data['arquivo']
-        model.nome=request.FILES ['arquivo']
-        model.save()
-        form= CsvModelForm()
-        obj= Csv.objects.get(ativo=False)
-        
-        with open(obj.arquivo.path, 'r') as f:
-            reader= csv.reader(f)
+    try:
+        if form.is_valid():        
+            model=Csv()
+            model.arquivo=form.cleaned_data['arquivo']
+            model.nome=request.FILES ['arquivo']
+            model.save()
+            form= CsvModelForm()
+            obj= Csv.objects.get(ativo=False)
+            
+            with open(obj.arquivo.path, 'r') as f:
+                reader= csv.reader(f)
 
-            for i, row in enumerate(reader):
-                if i==0:
-                    pass
-                else:
-                    telefone= row[1]
-                    pessoa=row[0]
-                    Contato.objects.create(
-                        nome=pessoa,
-                        contato= telefone,
-                    )
-            obj.ativo=True
-            obj.save()
-    return render (request, 'csvs/upload.html',{'form':form})
-
+                for i, row in enumerate(reader):
+                    if i==0:
+                        pass
+                    else:
+                        telefone= row[1]
+                        pessoa=row[0]
+                        Contato.objects.create(
+                            nome=pessoa,
+                            contato= telefone,
+                        )
+                obj.ativo=True
+                obj.save()
+        return render (request, 'csvs/upload.html',{'form':form})
+    except:
+        messages.error(request, "Já existe esse arquivo cadastrado")
+        return redirect ('/')
